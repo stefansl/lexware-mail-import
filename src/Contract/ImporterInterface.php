d852eb109aa3f492a48c44fc6298051f0593f1e1
stefansl <ssl@clickpress.de>
2025-11-06 19:11:13 +0100

    Add PHPDoc to ImporterInterface describing the runOnce method functionality

--- src/Contract/ImporterInterface.php
+++ src/Contract/ImporterInterface.php
@@ -5,6 +5,9 @@ namespace App\Contract;
 
 use App\DTO\ImapFetchFilter;
 
+/**
+ * Runs one import cycle: fetch mails, persist records, extract PDFs, upload to Lexware, update flags, notify on error.
+ */
 interface ImporterInterface
 {
     public function runOnce(?ImapFetchFilter $filter = null): void;
